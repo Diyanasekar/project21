020ad8a0f5d7d6f351fb1bb1a0f6f6be41e3bc94
Diyanasekar <diyanas1997@gmail.com>
2017-08-01 11:12:50 +0530

    Create Strint.java

---
+++ Strint.java
@@ -0,0 +1,14 @@
+ import java.io.*;
+ import java.util.*;
+ public class Strint
+ {
+   public static void main(String args[]) throws IOException
+   {
+          BufferedReader br =new BufferedReader(new InputStreamReader (System.in));
+          System.out.println("Enter the string");
+          String s="100";
+          int i=Integer.parseInt(s);
+          System.out.println(i+100);
+          }
+          }
+          
